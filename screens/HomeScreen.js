1db977892c030c017b29e47b1b3faf190b892214
Tishani Rathnasiri <tishani.20@cse.mrt.ac.lk>
2023-09-12 01:16:02 +0530

    Tishani's 4th commit with components of the item page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image, ScrollView, TextInput, ImageBackground} from 'react-native'
+import { View, Text, TouchableOpacity, Image, ScrollView, ImageBackground,FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {ArrowLeftIcon} from 'react-native-heroicons/solid'
@@ -17,14 +17,18 @@ const Product_1_1 = require('../assets/images/product_1_1.jpg');
 
 export default function HomeScreen() {
   const navigation = useNavigation();
-
   return (
     <ImageBackground
       source={backgroundImage} 
       style={{ flex: 1, backgroundColor: themeColors.bg }}
     >
     <View className="flex-1 bg-white" style={{backgroundColor: themeColors.bg}}>
-      <SafeAreaView className="flex">
+
+
+      <View className="flex-1 bg-white "
+        style={{borderTopLeftRadius: 40, borderTopRightRadius: 40,backgroundColor: 'rgba(255, 255, 255, 0.75)'}}
+      > 
+            <SafeAreaView className="flex">
         <View className="flex-row justify-start">
             <TouchableOpacity 
                 onPress={()=> navigation.goBack()}
@@ -33,21 +37,18 @@ export default function HomeScreen() {
                 <ArrowLeftIcon size="20" color="black" />
             </TouchableOpacity>
         </View>
-
-        
                
-        </SafeAreaView>
-        <View className="flex-1 bg-white px-8 pt-8"
-        style={{borderTopLeftRadius: 40, borderTopRightRadius: 40,backgroundColor: 'rgba(255, 255, 255, 0.75)'}}
-      >    
-      <View>
-        <View  className="flex-row justify-center">
-          <Image source={Logo} 
-          style={{width: 100, height: 100}} />
-        </View>
-        <Text className="text-semibold text-center mb-2">Your Trusted Partner {'\n'} in Agriculture Excellence</Text>
-      
-        </View>    
+        </SafeAreaView>   
+        <View className="flex-row items-center justify-center">
+          <View className="flex-1 ">
+            <Image source={Logo} className = "ml-20 w-16 h-16" />
+          </View>
+          <View className="flex-1">
+            <Text className="font-bold text-sm italic text-[#205526]  mb-2 ">
+              Your Trusted Partner in Agriculture Excellence
+            </Text>
+          </View>
+        </View>  
         
         <ScrollView style={{ flex: 1, backgroundColor: themeColors.bg }}>
         <SafeAreaView style={{ flex: 1 }}>
@@ -96,52 +97,77 @@ export default function HomeScreen() {
               <Text className="font-xl font-bold text-center text-black">
                   Equipments
               </Text>
-            </TouchableOpacity>
-            
+            </TouchableOpacity>           
         </View>
-
-        <Text className="text-2xl font-bold tracking-tight text-gray-900">
+        
+        <Text className="text-2xl font-bold tracking-tight text-center m-4">
             All Products
         </Text>
-
-        <View className="form space-y-2 justify-center items-center">
-            <TouchableOpacity className="py-3 bg-white w-36 rounded-xl">
-            <View  className="flex-row justify-center">
-              <Image source={Product_1} 
-              style={{width: 100, height: 100}} />
-            </View>
-              <Text className="font-xl font-bold text-center text-black">
-              Passion Fruit
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="py-3 bg-white w-36 rounded-xl">
-            <View  className="flex-row justify-center">
-              <Image source={Product_1_1} 
-              style={{width: 100, height: 100}} />
-            </View>
-              <Text className="font-xl font-bold text-center text-black">
-              Passion Fruit
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="py-3 bg-white w-36 rounded-xl">
-            <View  className="flex-row justify-center">
-              <Image source={Product_1} 
-              style={{width: 100, height: 100}} />
-            </View>
+        
+        <View className="flex flex-row justify-between m-4">
+          <View className="flex-1">
+          <TouchableOpacity onPress={()=> navigation.navigate('OneItem')}>
+            <View className="py-3 bg-white w-44 rounded-xl">
+              <View  className="flex-row justify-center">
+                <Image 
+                  source={Product_1} 
+                  style={{width: 100, height: 100}} 
+                />
+              </View>
               <Text className="font-xl font-bold text-center text-black">
                 Passion Fruit
               </Text>
+              </View>
             </TouchableOpacity>
-            <TouchableOpacity className="py-3 bg-white w-36 rounded-xl">
-            <View  className="flex-row justify-center">
-              <Image source={Product_1_1} 
-              style={{width: 100, height: 100}} />
-            </View>
+          </View>
+          <View className="flex-1">
+          <TouchableOpacity onPress={()=> navigation.navigate('OneItem')}>
+            <View className="py-3 bg-white w-44 rounded-xl">
+              <View  className="flex-row justify-center">
+                <Image 
+                  source={Product_1_1} 
+                  style={{width: 100, height: 100}} 
+                />
+              </View>
               <Text className="font-xl font-bold text-center text-black">
-                  Passion Fruit
+                Passion Fruit
               </Text>
+              </View>
             </TouchableOpacity>
-            
+          </View>
+        </View>
+
+        <View className="flex flex-row justify-between m-4">
+          <View className="flex-1">
+          <TouchableOpacity onPress={()=> navigation.navigate('OneItem')}>
+            <View className="py-3 bg-white w-44 rounded-xl">
+              <View  className="flex-row justify-center">
+                <Image 
+                  source={Product_1_1} 
+                  style={{width: 100, height: 100}} 
+                />
+              </View>
+              <Text className="font-xl font-bold text-center text-black">
+                Passion Fruit
+              </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View className="flex-1">
+          <TouchableOpacity onPress={()=> navigation.navigate('OneItem')}>
+            <View className="py-3 bg-white w-44 rounded-xl">
+              <View  className="flex-row justify-center">
+                <Image 
+                  source={Product_1} 
+                  style={{width: 100, height: 100}} 
+                />
+              </View>
+              <Text className="font-xl font-bold text-center text-black">
+                Passion Fruit
+              </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
 
         </SafeAreaView>
@@ -159,7 +185,5 @@ export default function HomeScreen() {
       </View>
     </View>
     </ImageBackground>
-       
-   
   )
 }
---
+++ screens/OneItemScreen.js
@@ -0,0 +1,103 @@
+import { View, Text, Image, TouchableOpacity,ScrollView } from 'react-native'
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {ArrowLeftIcon} from 'react-native-heroicons/solid';
+import { useNavigation } from '@react-navigation/native';
+
+const Product_1 = require('../assets/images/product_1.jpg');
+
+export default function OneItemScreen() {
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView>
+       <View className="flex-row ">
+            <TouchableOpacity 
+                onPress={()=> navigation.navigate('Home')}
+                className="bg-[#3da749] p-2 rounded-tr-2xl rounded-bl-2xl ml-4"
+>
+                <ArrowLeftIcon size="20" color="black" />
+            </TouchableOpacity>
+        </View>
+    
+      <ScrollView>
+        
+        {/* Box 1: Product Image */}
+        <View className="rounded bg-[#FAFAFA]">
+        <View className="p-2">
+          <View className="flex-1 ">
+            <Image source={Product_1} className = "w-full h-64" />
+          </View>
+        </View>
+        </View>
+
+        {/* Box 2: Product Info */}
+        <View className="rounded p-1 bg-[#FAFAFA] mx-2">
+        <View style={{ backgroundColor: 'lightgray', padding: 10 }}>
+          <Text className="text-2xl font-bold">Passion Fruit</Text>
+          <View>
+            <Text>Ratings</Text>
+          </View>
+          <Text className= "">Category : Fruit</Text>
+          <Text>Price: Rs. 548/kg</Text>
+          <Text>Quantity:</Text>
+          <Text className="mb-2 text-lg font-semibold">Description:</Text>
+          <Text className="mb-2 text-lg font-semibold">Specification:</Text>
+          <Text className="mb-2 text-lg font-semibold">Stock:</Text>
+          <TouchableOpacity className="py-3 bg-[#3da749] rounded-3xl">
+                <Text className="font-xl font-bold text-center text-white">
+                    Add to Cart
+                </Text>
+            </TouchableOpacity>
+         
+        </View>
+        </View>
+
+        {/* Box 3: Delivery Section */}
+        <View className="rounded p-1 bg-[#FAFAFA] mx-2">
+        <View style={{ backgroundColor: 'lightgray', padding: 10 }}>
+            <Text className="text-xl font-bold mb-2">Delivery</Text>
+            <View className="flex  mb-2">
+            <View className="mr-2">
+                {/* <FaMapMarkerAlt /> */}
+                </View>
+              <Text>Delivery Address</Text>
+              <TouchableOpacity>
+                <Text className="ml-auto text-blue-500">Change</Text>
+              </TouchableOpacity>
+              </View>
+            <View className="flex  mb-2">
+            <View className="mr-2">
+                {/* <FaTruck /> */}
+                </View>
+                <Text>Estimated Delivery Date</Text>
+              <Text className="ml-auto">Delivery Cost</Text>
+              </View>
+            <View className="flex ">
+            <View className="mr-2">
+                {/* <FaMoneyBillWave /> */}
+                </View>
+                <Text>Payment Method</Text>
+              <TouchableOpacity >
+                <Text className="ml-auto text-blue-500">Change</Text></TouchableOpacity>
+              </View>
+              </View>    
+        </View>
+      
+
+        {/* Box 4: Shop Information */}
+        <View className="rounded p-1 bg-[#FAFAFA] mx-2">
+        <View style={{ backgroundColor: 'lightgray', padding: 10 }}>
+          <Text className="text-xl font-bold mb-2">Shop Name</Text>
+          <TouchableOpacity className="py-3 bg-[#3da749] rounded-3xl">
+                <Text className="font-xl font-bold text-center text-white">
+                    Visit Store
+                </Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+      </ScrollView>
+    
+    </SafeAreaView>
+    
+  )
+}
